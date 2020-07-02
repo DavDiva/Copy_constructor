70d10a05cf1bb0f4b1d8c7c02b88e23a8c8282f2
Diana Ivanchevska <ivanchevska.diana@gmail.com>
2020-07-02 21:27:11 +0300

    Overload operator function

--- Copy_constructor.cpp
+++ Copy_constructor.cpp
@@ -5,10 +5,10 @@ class MyClass
 {
 private:
 	int size;
-public:
 	int* pointer_data;
-
-	MyClass(int size)
+public:
+	
+	MyClass(int size)//constructor with arguments
 	{
 		this->size = size;
 		this->pointer_data = new int[size];
@@ -18,7 +18,7 @@ public:
 		}
 		cout << "Constructor called " <<this<< endl;
 	}
-	MyClass(const MyClass& other)
+	MyClass(const MyClass& other)//Copy constructor
 	{
 		this->size = other.size;
 		this->pointer_data = new int[other.size];
@@ -27,6 +27,23 @@ public:
 			this->pointer_data[i] = other.pointer_data[i];
 		}
 		cout << "Copy Constructor called " <<this<< endl;
+
+	}
+	MyClass& operator = (const MyClass& other)//Overload operator function
+	{
+		this->size = other.size;
+		if (this->pointer_data != nullptr)
+		{
+			delete[] this->pointer_data;
+		}
+		this->pointer_data = new int[size];
+		for (int i = 0; i < size; i++)
+		{
+			this->pointer_data[i] = other.pointer_data[i];
+		}
+		cout << "Function Operator = Overload called." << endl;
+
+		return *this;
 
 	}
 	~MyClass()
@@ -48,11 +65,23 @@ public:
 int main()
 {
 	MyClass a(10);
-	MyClass b(a);
+	/*MyClass b(a);*/
+	MyClass b(3);
+	MyClass c(6);
 	cout << "Array a: " << endl;
 	a.GetData();
 	cout << "Array b: " << endl;
 	b.GetData();
+	cout << "Array c: " << endl;
+	c.GetData();
+	a = b = c;
+	cout << "__________________________" << endl;
+	cout << "Array a: " << endl;
+	a.GetData();
+	cout << "Array b: " << endl;
+	b.GetData();
+	cout << "Array c: " << endl;
+	c.GetData();
 	return 0;
 }
 
